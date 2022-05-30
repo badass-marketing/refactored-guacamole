f05e34aaa7eeff07bcba2d9a8dfe719351028d67
badass-marketing <amtarget1512@gmail.com>
2022-05-30 20:06:19 +0300

    5th Homework, check please.

---
+++ 5_homework.py
@@ -0,0 +1,31 @@
+
+
+
+
+# a = int(input("Input numbers, then hit 'Space/Enter:' "))
+
+
+sum = 0
+count = 0
+max = 0
+min = None
+arithmetic_mean = 0
+# , count, max, min, arithmetic_mean
+while True:
+    s = input("Enter a number, then show your ☞ 😉HATE⏎😁 for 💻apple🍏 : ")
+    if not s:
+        print("Сумма чисел: ", sum)
+        print("Количество чисел: ", count)
+        print("Максимальное число: ", max)
+        print("Минимальное число: ", min)
+        print("Среднее арифметическое: ", arithmetic_mean)
+        break
+    number = int(s)
+    sum += number
+    count += 1
+    arithmetic_mean = int(sum / count)
+    if number > max:
+        max = number
+    if min is None or number < min:
+        min = number
+
