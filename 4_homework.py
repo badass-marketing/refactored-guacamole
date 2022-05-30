b58e9369da8cfb5a6f9762e413bc865d7c1996c2
badass-marketing <amtarget1512@gmail.com>
2022-05-30 12:28:09 +0300

    check please 4th homework

---
+++ 4_homework.py
@@ -0,0 +1,29 @@
+
+
+
+n = int(input("Введи число: "))
+
+k = 0
+for i in range(2, n // 2+1):
+    if (n % i == 0):
+        k = k+1
+if (k <= 0):
+    print("Число простое")
+else:
+    print("Число не является простым")
+
+print("Список простых чисел: ")
+
+def p(x):
+	for d in range(2, int(x ** 0.5) + 1):
+		if x % d == 0:
+			return False
+		else:
+			return True
+
+
+for i in range(10, n):
+	if p(i) == True:
+		print(i)
+
+
